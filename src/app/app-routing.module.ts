471b0207ffca731404adcda4a3ce2b5c2790f9b5
JoshCyril <joshua.cyril88@gmail.com>
2023-03-20 14:41:14 +0530

    ng c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { ProjectComponent } from './project/project.component';
+import { ResourceComponent } from './resource/resource.component';
+import { ResumeComponent } from './resume/resume.component';
+import { V1Component } from './version/v1/v1.component';
+import { V2Component } from './version/v2/v2.component';
 
-const routes: Routes = [];
+const appNameTitle = "JoshCyril"
+
+const routes: Routes = [
+  {
+    path: "",
+    title: appNameTitle + ' - Portfolio',
+    component: HomeComponent
+  },
+  {
+    path: "project",
+    title: appNameTitle + ' - Project',
+    component: ProjectComponent
+  },
+  {
+    path: "resume",
+    title: appNameTitle + ' - Resume',
+    component: ResumeComponent
+  },
+  {
+    path: "resource",
+    title: appNameTitle + ' - Resource',
+    component: ResourceComponent
+  },
+  {
+    path: "version/v1",
+    title: appNameTitle + ' - Version 1',
+    component: V1Component
+  },
+  {
+    path: "version/v2",
+    title: appNameTitle + ' - Version 2',
+    component: V2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './home/about/about.component';
+import { StatusComponent } from './home/status/status.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectComponent } from './project/project.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ResourceComponent } from './resource/resource.component';
+import { V1Component } from './version/v1/v1.component';
+import { V2Component } from './version/v2/v2.component';
+import { LoaderComponent } from './home/loader/loader.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    StatusComponent,
+    ContactComponent,
+    ProjectComponent,
+    ResumeComponent,
+    ResourceComponent,
+    V1Component,
+    V2Component,
+    LoaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
